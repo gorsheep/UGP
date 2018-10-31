48c93b1f3398d46551a69453f753a35c80e3e228
Даниил Волошин <kuravasic@gmail.com>
2018-10-31 20:14:04 +0300

    Initial Commit

---
+++ UGP build 001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UGP build 001
+//
+//  Created by Даниил Волошин on 10/31/18.
+//  Copyright © 2018 Даниил Волошин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
